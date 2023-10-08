17e2476e7b6e52c0efa81e565ba4cd15b0372ffb
m-honarzade <honarzadeh.mahdie@gmail.com>
2023-10-08 13:14:29 +0330

    adding elements and  convert functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,81 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [amountInput, setAmountInput] = useState("1");
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const amountInputHandler = (e) => {
+    setAmountInput(+e.target.value);
+  };
+
+  useEffect(() => {
+    const transformCurrency = async () => {
+      const host = "api.frankfurter.app";
+      setIsLoading(true);
+
+      const res = await fetch(
+        `https://${host}/latest?amount=${+amountInput}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+      setIsLoading(false);
+      setOutput(data.rates[toCurrency]);
+    };
+
+    if (fromCurrency === toCurrency) return setOutput("1");
+    transformCurrency();
+  }, [amountInput, fromCurrency, toCurrency]);
+
+  return (
+    <>
+      <div className="   border border-purple-700 border-1 rounded-md max-w-2xl h-40 mx-auto mt-20 text-center">
+        <div className="flex-col flex justify-center items-center p-12">
+          <div className="flex flex-row gap-x-2">
+            <input
+              type="text"
+              placeholder="amount"
+              value={amountInput}
+              onChange={amountInputHandler}
+              disabled={isLoading}
+              className=" p-2  text-sm w-40 border border-purple-400 "
+            />
+            <select
+              value={fromCurrency}
+              onChange={(e) => setFromCurrency(e.target.value)}
+              disabled={isLoading}
+              className="w-24 border border-purple-400 text-gray-500 text-sm "
+            >
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="INR">INR</option>
+              <option value="CAD">CAD</option>
+            </select>
+            <select
+              value={toCurrency}
+              onChange={(e) => setToCurrency(e.target.value)}
+              disabled={isLoading}
+              className="w-24 border border-purple-400 text-gray-500 text-sm "
+            >
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="INR">INR</option>
+              <option value="CAD">CAD</option>
+            </select>
+          </div>
+          <div className="mt-6">
+            <p className="text-purple-700 text-sm font-semibold">
+              {amountInput}{" "}
+              <span className="text-gray-500 text-xs">{fromCurrency}</span> ==={" "}
+              {output}
+              <span className="text-gray-500 text-xs"> {toCurrency}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
